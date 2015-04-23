428650a0dbe558d7cd4f18d195e51b74b9b44eb1
Nomorewine <choi.al17@gmail.com>
2015-04-23 15:51:53 +0900

    added launcher activity to android manifest, added layout to main activity, and onCreate to main.java

---
+++ app/src/main/java/com/boshetoonmai/secretgame/BitmapActivity.java
@@ -0,0 +1,15 @@
+package com.boshetoonmai.secretgame;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class BitmapActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitmap);
+    }
+
+}
--- app/src/main/java/com/boshetoonmai/secretgame/Main.java
+++ app/src/main/java/com/boshetoonmai/secretgame/Main.java
@@ -1,14 +1,38 @@
 package com.boshetoonmai.secretgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main extends Activity{
+public class Main extends Activity implements View.OnClickListener{
 
+    Button bitmap_activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        bitmap_activity = (Button) findViewById(R.id.bitmap_btn);
+        bitmap_activity.setOnClickListener(this);
+    }
+
+    /**
+     *
+     * Tut sozdavay onClick event handlery dlya svoey knopki.
+     *
+     * **/
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bitmap_btn:
+                Intent bitmapIntent = new Intent(this, BitmapActivity.class);
+                startActivity(bitmapIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
