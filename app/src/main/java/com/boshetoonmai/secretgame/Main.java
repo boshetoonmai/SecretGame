b71613d4840c6ecc2c1c3b53fa3a9b151b2598dd
Nomorewine <choi.al17@gmail.com>
2015-04-23 11:26:04 +0900

    added launcher activity to android manifest, added layout to main activity, and onCreate to main.java

--- app/src/main/java/com/boshetoonmai/secretgame/Main.java
+++ app/src/main/java/com/boshetoonmai/secretgame/Main.java
@@ -1,9 +1,14 @@
 package com.boshetoonmai.secretgame;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Main extends Activity{
 
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
 }
